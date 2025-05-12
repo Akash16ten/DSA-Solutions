f020f2f4a5340c775d176c8e06e3d88000140796
Akash16ten <193934829+Akash16ten@users.noreply.github.com>
2025-05-12 19:12:44 +0530

    Solution to the problem

---
+++ Arrays/Stock_Buy_and_Sell.cpp
@@ -0,0 +1,22 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Buy on day 2, sell on day 5
+*/
+
+int maxProfit(vector<int>& prices) {
+    int minPrice = INT_MAX;
+    int maxProfit = 0;
+
+    for (int price : prices) {
+        minPrice = min(minPrice, price);                      
+        maxProfit = max(maxProfit, price - minPrice);         
+    }
+    
+    return maxProfit;
+}
