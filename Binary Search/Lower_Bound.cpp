8b026feb981c94275c210db30ab518f19e65bf57
Akash16ten <193934829+Akash16ten@users.noreply.github.com>
2025-06-06 19:22:39 +0530

    Update Lower_Bound.cpp

--- Binary Search/Lower_Bound.cpp
+++ Binary Search/Lower_Bound.cpp
@@ -1,1 +1,9 @@
-// In a given array nums, find the smallest index, such that nums[i] >= target
+/*
+We have an integer target, in a given array nums, find the smallest index, such that nums[i] >= target
+
+Example:
+nums = [3 5 8 15 19]
+target = 8
+Output = 2 
+*/
+
