a05ffdb6428b090aae4708017398063ab4f8f058
Akash16ten <193934829+Akash16ten@users.noreply.github.com>
2025-06-06 19:40:03 +0530

    Create Upper_Bound.cpp

---
+++ Binary Search/Upper_Bound.cpp
@@ -0,0 +1,23 @@
+/*
+We have an integer target, in a given array nums, find the smallest index, such that nums[i] > target
+
+Example:
+nums = [3 5 8 15 19]
+target = 8
+Output = 2 
+*/
+
+int left = 0, right = nums.size() - 1, answer = nums.size();
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] > target)
+            {
+              answer = mid;
+              left = mid + 1;
+            }  
+            else
+              left = mid + 1;
+        }
+        return amswer; 
+    }
