3548cc312e163b82e998a8be0314d62278e3a23b
Akash16ten <193934829+Akash16ten@users.noreply.github.com>
2025-07-20 23:49:00 +0530

    Solution to the problem.

--- Binary Search/Insert_Position.cpp
+++ Binary Search/Insert_Position.cpp
@@ -9,3 +9,16 @@ Input: nums = [1,3,5,6], target = 5
 Output: 2
 */
 
+int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return left; 
+    }
