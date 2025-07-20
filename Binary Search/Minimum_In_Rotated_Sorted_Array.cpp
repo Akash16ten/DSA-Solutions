6f1c1b024756498e43d05b23cac95e59e3666b32
Akash16ten <193934829+Akash16ten@users.noreply.github.com>
2025-07-21 00:09:54 +0530

    Solution to the problem.

--- Binary Search/Minimum_In_Rotated_Sorted_Array.cpp
+++ Binary Search/Minimum_In_Rotated_Sorted_Array.cpp
@@ -13,3 +13,19 @@ Example:
 Input: nums = [3,4,5,1,2]
 Output: 1
 */
+
+int findMin(vector<int>& nums) {
+        int n= nums.size();
+        int low=0, high= n-1;
+        int mini = INT_MAX;
+
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            mini = min(nums[mid], mini);
+
+            if (nums[mid] < nums[high]) high= mid-1;
+            else low= mid+1;
+        }
+    return mini;
+    }
