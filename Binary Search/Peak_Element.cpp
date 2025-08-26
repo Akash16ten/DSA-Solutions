69aa7d0faa0c8a1dca1b5c6fdc3cf7e40e02a88d
Akash16ten <193934829+Akash16ten@users.noreply.github.com>
2025-08-26 23:17:41 +0530

    Solution to the problem.

--- Binary Search/Peak_Element.cpp
+++ Binary Search/Peak_Element.cpp
@@ -8,6 +8,12 @@ You may imagine that nums[-1] = nums[n] = -∞.
 In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
 
 You must write an algorithm that runs in O(log n) time.
+
+
+Example:
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
 */
 
 int findPeakElement(vector<int>& nums) {
