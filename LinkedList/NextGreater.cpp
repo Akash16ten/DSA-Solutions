ee5d7b4adbdf2493e6076230eb7e2ae98ff6fed6
Akash16ten <193934829+Akash16ten@users.noreply.github.com>
2025-11-13 14:20:06 +0530

    Solution to the problem.
    Implement a function to find the next greater node for each node in a linked list.

--- LinkedList/NextGreater.cpp
+++ LinkedList/NextGreater.cpp
@@ -5,3 +5,33 @@ For each node in the list, find the value of the next greater node. That is, for
 
 Return an integer array answer where answer[i] is the value of the next greater node of the ith node (1-indexed). If the ith node does not have a next greater node, set answer[i] = 0.
 */
+
+
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        ListNode* temp = head;
+        vector<int> answer;
+
+        while(temp != NULL)
+        {
+            ListNode* dog = temp -> next;
+            while (dog != NULL)
+            {
+                if (dog->val > temp->val) 
+                {
+                    answer.push_back(dog->val);
+                    break;
+                }
+
+                else
+                dog = dog -> next; 
+            }
+
+            if (dog == NULL) answer.push_back(0);
+
+            temp = temp -> next;
+        }
+    return answer;
+    }
+};
