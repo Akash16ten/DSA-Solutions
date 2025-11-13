f241cfc903ad7c67bbaadd5bf789d1ee53571d55
Akash16ten <193934829+Akash16ten@users.noreply.github.com>
2025-11-13 14:13:19 +0530

    Solution to the problem.
    Implemented the mergeNodes function to sum values between consecutive zeros in a linked list.

--- LinkedList/MergeNodes.cpp
+++ LinkedList/MergeNodes.cpp
@@ -5,3 +5,46 @@ For every two consecutive 0's, merge all the nodes lying in between them into a 
 
 Return the head of the modified linked list.
 */
+
+
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+
+        if (head == NULL || head -> next == NULL) return head;
+
+        ListNode* temp = head->next;
+        int hnodekivalue = 0;
+
+        while (temp -> val != 0)
+        {
+            hnodekivalue += temp -> val;
+            temp = temp -> next;
+        }
+
+        ListNode* newhead = new ListNode(hnodekivalue);
+        newhead -> next = NULL;
+        // to ye node ab hamara head ban gya
+        ListNode* cat = newhead;
+        // kyoki apne head ko hame tamper nhi krna chahiye, hamne use ek naya
+        // node bana kr store krdiya
+
+        int nodekivalue = 0;
+        while (temp -> next != NULL) 
+        {   
+            temp = temp -> next;
+            while (temp -> val != 0)
+            {
+                nodekivalue += temp -> val;
+                temp = temp -> next;
+            }
+
+            ListNode* dog = new ListNode(nodekivalue);
+            cat -> next = dog;
+            dog -> next = NULL;
+            cat = cat -> next;
+            nodekivalue = 0;
+        }
+    return newhead;
+    }
+};
