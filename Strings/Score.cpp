b0f6a7bc6ce2466ad109d04f6b4343a7793087c4
Akash16ten <193934829+Akash16ten@users.noreply.github.com>
2025-11-13 14:35:04 +0530

    Solution to the problem.
    Implement score calculation for a string based on ASCII differences.

--- Strings/Score.cpp
+++ Strings/Score.cpp
@@ -3,3 +3,15 @@ You are given a string s. The score of a string is defined as the sum of the abs
 
 Return the score of s.
 */
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int answer= 0;
+        for (int i=0; i<s.size()-1; i++)
+        {
+            answer+= abs(s[i]-s[i+1]);
+        }
+    return answer;
+    }
+};
